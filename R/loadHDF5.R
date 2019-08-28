d3aa2b9f9b34acbe06b1fbfd82916377abe64c39
Morrison <jacob.morrison@submit.cm.cluster>
2019-08-28 14:37:01 -0400

    Refactoring loadHDF5.R

--- R/loadHDF5.R
+++ R/loadHDF5.R
@@ -1,14 +1,20 @@
-#' a simple local wrapper around HDF5Array::loadHDF5SummarizedExperiment
+#' Function to load HDF5-backed SummarizedExperiment from read.biscuit
+#'
+#' Local wrapper for HDF5Array::loadHDF5SummarizedExperiment
 #' 
-#' @param   dir   path to the file assays.h5 (and se.rds); default is "HDF5"
+#' @param   dir     path to the HDF5-backed file
+#' @param   prefix  optional prefix to add to filenames inside ‘dir’; default is ""
 #' 
 #' @return        a SummarizedExperiment-derived object (such as a BSseq object)
 #' 
 #' @import HDF5Array
 #' 
 #' @export
-loadHDF5 <- function(dir="HDF5") {
-  # lop off the filename if it is supplied
-  dir <- base::sub("assays.h5$", "", dir)
-  loadHDF5SummarizedExperiment(dir=dir)
+#'
+loadHDF5 <- function(dir, prefix = "") {
+    # Check directory exists before running load function
+    if (!dir.exists(dir)) {
+        stop(dir, " does not exist. Enter a valid directory")
+    }
+    loadHDF5SummarizedExperiment(dir = dir, prefix = prefix)
 }
